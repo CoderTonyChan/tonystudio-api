c9b1a2db58ac73fd376f8542fc975304fb2f3d09
CoderTonyChan <101992315@qq.com>
2019-03-29 15:49:25 +0800

    update

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     airtable: {
-        appkey: process.env.airtable || 'key3RsANRfuAozqgP', // airtable
+        appkey: process.env.airtable || 'key3RsANRTonyfuAozqgP', // airtable
         tableURL: process.env.tableURL || 'appSyHuGwMS7p7X1s',
     },
     douban: {
-        user: 103961302, // airtable
+        user: 103961302,
     },
 };
